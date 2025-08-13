d30d43dd2b3f0b6df4917d6455cef97d63a42e7f
Lena <_@example.c git config --global user.email>
2025-08-13 20:22:42 +0300

    L4

--- 05_lesson/lesson5_task3.py
+++ 05_lesson/lesson5_task3.py
@@ -8,8 +8,8 @@ driver.get("http://the-internet.herokuapp.com/inputs")
 input_field = driver.find_element(By.TAG_NAME, "input")
 
 
-input_field.send_keys('1000')
+input_field.send_keys("Sky")
 input_field.clear()
-input_field.send_keys('900')
+input_field.send_keys("Pro")
 
 driver.quit()
--- 05_lesson/lesson5_task4.py
+++ 05_lesson/lesson5_task4.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(options=options)
 
@@ -8,10 +9,12 @@ driver.get("http://the-internet.herokuapp.com/login")
 input_field = driver.find_element(By.TAG_NAME, "input")
 
 
-input_field.send_keys('tomsmith')
-input_field.send_keys('SuperSecretPassword!')
-button = driver.find_element(By.CSS_SELECTOR, "Login")
-button.click()
+search_box = driver.find_element(By.NAME, "username")
+search_box.send_keys('tomsmith')
+search_box = driver.find_element(By.NAME, "password")
+search_box.send_keys('SuperSecretPassword!')
+
+driver.find_element(By.LINK_TEXT, "Login"). click()
 
 print("fork_me_on_github")
 
