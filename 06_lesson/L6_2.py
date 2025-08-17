ff8718491f3468d4fcf35ff03b430d280b70f5ea
Lena <_@example.c git config --global user.email>
2025-08-17 22:27:00 +0300

    L6

--- 06_lesson/L6_2.py
+++ 06_lesson/L6_2.py
@@ -7,19 +7,19 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/textinput")
 
-WebDriverWait(driver, 35).until(EC.text_to_be_present_in_element((By.ID, 'updatingButton'), 'SkyPro'))
+#input_field = driver.find_element(By.TAG_NAME, 'input')
 
+input = driver.find_element(By.ID, 'newButtonName')
+input.send_keys('SkyPro')
 
-input_field = driver.find_element(By.TAG_NAME, 'input')
-
-search_box = driver.find_element(By.ID, 'newButtonName')
-search_box.send_keys('SkyPro')
-
+#WebDriverWait(driver, 35).until(EC.text_to_be_present_in_element((By.ID, 'newButtonName'), 'SkyPro'))
 
 button = driver.find_element(By.ID, 'updatingButton')
 button.click()
 
-print(button.text)
+WebDriverWait(driver, 35).until(EC.text_to_be_present_in_element((By.ID, 'updatingButton'), 'SkyPro'))
+
+txt = driver.find_element(By.ID, 'updatingButton')
+print(txt)
 
 driver.quit()
-
